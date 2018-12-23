f97886f99aa9d7a4a4a9dd96bd7036698c8a0bd5
unknown <idzer0lis@gmail.com>
2018-12-23 18:18:46 +0200

    build fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        
       ],
       declarations: [
         AppComponent
@@ -20,21 +19,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'web-calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('web-calculator');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to web-calculator!');
-  });
   it('should calculate a result with the provided operation and operands', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.nativeElement;
+    const app = fixture.debugElement.componentInstance;
     app.firstOperand = 1;
     app.secondOperand = 2;
     app.operation = '+';
