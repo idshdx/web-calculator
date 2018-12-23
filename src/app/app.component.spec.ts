16a824ede06f9358ce66284e33b9e0bbe688f2ee
unknown <idzer0lis@gmail.com>
2018-12-23 18:52:33 +0200

    added some tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,14 +19,62 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should calculate a result with the provided operation and operands', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  describe('should calculate a result with the provided operation and operands', () => {
+    it('+', ()=> {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      app.firstOperand = 1;
+      app.secondOperand = 2;
+      app.operation = '+';
+      app.displayResult();
+      fixture.detectChanges();
+      expect(app.result).toBe(3);
+    });
+    it('-', ()=> {
+     const fixture = TestBed.createComponent(AppComponent);
+const app = fixture.debugElement.componentInstance;
+      app.firstOperand = 1;
+      app.secondOperand = 2;
+      app.operation = '-';
+      app.displayResult();
+      fixture.detectChanges();
+      expect(app.result).toBe(-1);
+    });
+    it('*', ()=> {
+   const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.firstOperand = 1;
+    app.secondOperand = 2;
+    app.operation = '*';
+    app.displayResult();
+    fixture.detectChanges();
+    expect(app.result).toBe(2);
+    });
+    it('/', ()=> {
+         const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.firstOperand = 1;
+    app.secondOperand = 2;
+    app.operation = '/';
+    app.displayResult();
+    fixture.detectChanges();
+    expect(app.result).toBe(0.5);
+    });
+  });
+  describe('go back to previous results', async()=> {
+           const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     app.firstOperand = 1;
     app.secondOperand = 2;
     app.operation = '+';
     app.displayResult();
     fixture.detectChanges();
+    app.secondOperand = 1;
+    app.operation = '+';
+    app.displayResult();
+    fixture.detectChanges();
+    app.goBack();
+    fixture.detectChanges();
     expect(app.result).toBe(3);
-  });
+  })
 });
